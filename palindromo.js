a9972a004f8c435df2376d23d1155d2bf8927765
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-03 14:31:56 -0300

    Update palindromo.js

--- palindromo.js
+++ palindromo.js
@@ -1,8 +1,8 @@
-function palindromo(str){
+function palindromo(string){
     //remove os espaços e converte para minúscula
-    str = str.replace(/\s+/g, '').toLowerCase();
+    string = string.replace(/\s+/g, '').toLowerCase();
     //verfica se a string é igual a sua reversa
-    return str === str.split('').reverse().join('');
+    return string === string.split('').reverse().join('');
 }
 
 module.exports = palindromo;
